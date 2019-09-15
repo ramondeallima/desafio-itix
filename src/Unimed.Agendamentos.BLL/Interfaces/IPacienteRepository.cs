67da8b976cb0f2a7250e290157204cced8530003
Ramon Almeida <ramondeallima@gmail.com>
2019-09-15 20:57:56 -0300

    Refatoração de código

--- src/Unimed.Agendamentos.BLL/Interfaces/IPacienteRepository.cs
+++ src/Unimed.Agendamentos.BLL/Interfaces/IPacienteRepository.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using UnimedAgendamentos.BLL.Models;
 
 namespace Unimed.Agendamentos.BLL.Interfaces
 {
     public interface IPacienteRepository : IRepository<Paciente>
     {
+        Task<Paciente> ObterPacienteAgendamentos(Guid id);
     }
 }
--- src/Unimed.Agendamentos.DAL/Repository/PacienteRepository.cs
+++ src/Unimed.Agendamentos.DAL/Repository/PacienteRepository.cs
@@ -4,12 +4,20 @@ using System.Text;
 using UnimedAgendamentos.BLL.Models;
 using UnimedAgendamentos.DAL.Context;
 using Unimed.Agendamentos.BLL.Interfaces;
-
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Unimed.Agendamentos.DAL.Repository
 {
     public class PacienteRepository : Repository<Paciente>, IPacienteRepository
     {
         public PacienteRepository(UnimedAgendamentosDbContext context) : base(context) { }
+
+        public async Task<Paciente> ObterPacienteAgendamentos(Guid id)
+        {
+            return await Db.Pacientes.AsNoTracking()
+                .Include(c => c.Agendamentos)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
     }
 }
